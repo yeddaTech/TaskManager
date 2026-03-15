1e4ac85c330f469b8dbe01b0b4c540ab76cc37e4
yeddaTech <esyoun70@gmail.com>
2026-03-15 21:52:00 +0100

    Tailwind UI integration

--- templates/pages_templ.go
+++ templates/pages_templ.go
@@ -47,7 +47,7 @@ func Index() templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = Layout("Tailwind+Go demo").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = Layout("Task Manager").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
